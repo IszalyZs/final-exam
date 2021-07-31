b17b566791aeed76f6851db887a5939d493a7da9
IszalyZs <iszalyzs@gmail.com>
2021-07-31 18:40:23 +0200

    Add fixed dogControllerIT

---
+++ src/test/java/com/company/trexshelter/controller/BreedControllerIT.java
@@ -0,0 +1,28 @@
+package com.company.trexshelter.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreedControllerIT {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+}
