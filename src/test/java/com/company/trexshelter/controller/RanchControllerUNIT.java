5b1c960b37e08a4b888c668600930f0928060532
IszalyZs <iszalyzs@gmail.com>
2021-08-01 15:13:00 +0200

    Add  fixed RanchControllerUNIT

--- src/test/java/com/company/trexshelter/controller/RanchControllerUNIT.java
+++ src/test/java/com/company/trexshelter/controller/RanchControllerUNIT.java
@@ -2,22 +2,25 @@ package com.company.trexshelter.controller;
 
 import com.company.trexshelter.model.dto.RanchDTO;
 import com.company.trexshelter.service.RanchService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @WebMvcTest(RanchController.class)
 class RanchControllerUNIT {
@@ -28,9 +31,16 @@ class RanchControllerUNIT {
     @MockBean
     private RanchService ranchService;
 
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
-    void findAll() throws Exception {
+    void findAll_shouldReturnAllRanchDTOs() throws Exception {
         RanchDTO ranchDTO1 = new RanchDTO();
         ranchDTO1.setId(1L);
         ranchDTO1.setName("Trex Farm");
@@ -48,10 +58,13 @@ class RanchControllerUNIT {
                 .andExpect(jsonPath("$[1].id", is(ranchDTO2.getId().intValue())))
                 .andExpect(jsonPath("$[1].name", is(ranchDTO2.getName())))
                 .andExpect(jsonPath("$[1].address", is(ranchDTO2.getAddress())));
+        verify(ranchService, times(1))
+                .findAll();
+
     }
 
     @Test
-    void findById() throws Exception {
+    void findById_inputValidId_shouldReturnRightRanchDTO() throws Exception {
         RanchDTO ranchDTO1 = new RanchDTO();
         ranchDTO1.setId(1L);
         ranchDTO1.setName("Trex Farm");
@@ -60,15 +73,19 @@ class RanchControllerUNIT {
         ranchDTO2.setId(2L);
         ranchDTO2.setName("Trex Farm");
         ranchDTO2.setAddress("2222 Szeged Anna str. 26.");
-        when(ranchService.findById(2L)).thenReturn(ranchDTO2);
-        mockMvc.perform(get("/ranch/2")).andExpect(status().isOk())
+        when(ranchService.findById(anyLong())).thenReturn(ranchDTO2);
+        Long id = 2L;
+        mockMvc.perform(get("/ranch/{id}", id)).andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(ranchDTO2.getId().intValue())))
                 .andExpect(jsonPath("$.name", is(ranchDTO2.getName())))
                 .andExpect(jsonPath("$.address", is(ranchDTO2.getAddress())));
+
+        verify(ranchService, times(1))
+                .findById(id);
     }
 
     @Test
-    void findAllByRanchName() throws Exception {
+    void findAllByRanchName_inputValidRanchName_shouldReturnRightRanches() throws Exception {
         RanchDTO ranchDTO1 = new RanchDTO();
         ranchDTO1.setId(1L);
         ranchDTO1.setName("Trex Farm");
@@ -77,22 +94,47 @@ class RanchControllerUNIT {
         ranchDTO2.setId(2L);
         ranchDTO2.setName("Trex Hospital");
         ranchDTO2.setAddress("2222 Szeged Anna str. 26.");
-        when(ranchService.findAllByRanchName("Trex Farm")).thenReturn(List.of(ranchDTO1));
-        mockMvc.perform(get("/ranch/Trex Farm/ranch")).andExpect(status().isOk())
+        String name = "Trex Farm";
+        when(ranchService.findAllByRanchName(anyString())).thenReturn(List.of(ranchDTO1));
+        mockMvc.perform(get("/ranch/{name}/ranch", name)).andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id", is(ranchDTO1.getId().intValue())))
                 .andExpect(jsonPath("$[0].name", is(ranchDTO1.getName())))
                 .andExpect(jsonPath("$[0].address", is(ranchDTO1.getAddress())));
+
+        verify(ranchService, times(1))
+                .findAllByRanchName(anyString());
+
     }
 
     @Test
-    void deleteById() {
+    void deleteById_inputValidId() throws Exception {
+        RanchDTO ranchDTO1 = new RanchDTO();
+        ranchDTO1.setId(1L);
+        ranchDTO1.setName("Trex Farm");
+        ranchDTO1.setAddress("1111 Budapest Andrassy str.25.");
+        mockMvc.perform(delete("/ranch/{id}", anyLong()))
+                .andExpect(status().isOk());
+        verify(ranchService, times(1))
+                .deleteById(anyLong());
     }
 
     @Test
-    void save() {
+    void save_inputRightRanchDTO_shouldRanchDTO() throws Exception {
+        RanchDTO ranchDTO = new RanchDTO();
+        ranchDTO.setName("Trex Farm");
+        ranchDTO.setAddress("1111 Budapest Andrassy str.25.");
+        when(ranchService.save(any(RanchDTO.class))).thenReturn(ranchDTO);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/ranch")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(asJsonString(ranchDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Trex Farm")))
+                .andExpect(jsonPath("$.address", equalTo("1111 Budapest Andrassy str.25.")));
+
+        verify(ranchService, times(1))
+                .save(any(RanchDTO.class));
     }
 
-    @Test
-    void update() {
-    }
 }
